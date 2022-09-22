8c8044b2524c4298066adee14b861ab728720758
garion.musetta <garion.musetta@sysdata.it>
2022-09-22 14:49:03 +0200

    Update quiz model

--- src/db_setup.py
+++ src/db_setup.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = quiz_config.db_connection_string
 db = SQLAlchemy(app)
 
+# Info utente
 @dataclass
 class User(db.Model):
     id: int
@@ -17,16 +18,33 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), nullable=False)
 
+# Schema quiz
 @dataclass
 class Quiz(db.Model):
     id: int
+    question: str
+    question_id: str
+    category: str
+    
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(250), nullable=False)
+    question_id = db.Column(db.String(10), unique=True, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+
+# Risposte degli utenti al quiz
+@dataclass
+class UserQuiz(db.Model):
+    id: int
     user_id: int
+    question_id: str
     answer: str
     
     id = db.Column(db.Integer, primary_key=True)
-    answer = db.Column(db.JSON, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
+    question_id = db.Column(db.String(10), db.ForeignKey('quiz.question_id'), nullable=False)
+    answer = db.Column(db.String(10), nullable=False)
+
+# Distanze calcolate fra utenti
 @dataclass
 class Distance(db.Model):
     user1_id: int
