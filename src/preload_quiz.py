3b79b56eb713519762dd2c0f27f4aff23f8893d9
garion.musetta <garion.musetta@sysdata.it>
2022-09-27 16:36:40 +0200

    Preload schema

--- src/preload_quiz.py
+++ src/preload_quiz.py
@@ -2,14 +2,13 @@ from db_setup import db, Quiz
 import json
 
 
-schema_json = "data/risposte_elias.json"
-# TODO: aggiungere categorie
-category = "1"
+schema_json = "data/schema_quiz.json"
 
 if __name__ == "__main__":
     # TODO creare file schema.json
     schema = json.load(open(schema_json))
-    quiz = [ Quiz(question=question["question"], question_id=question["id"], category=category) for question in schema ]
+    for category in schema:
+        quiz = [ Quiz(question=question["question"], question_id=question["id"], category=category["category"]) for question in category["quiz"] ]
     
     db.session.add_all(quiz)
     db.session.commit()
