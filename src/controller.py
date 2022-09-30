4eb1572ab85e8212a40688b28eef81e962697491
garion.musetta <garion.musetta@sysdata.it>
2022-09-30 16:45:28 +0200

    Http error handling

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,5 @@
 from flask import jsonify, request, abort, Response
+from werkzeug.exceptions import HTTPException
 from db_setup import db, app, User, Quiz, Distance
 from quiz_config import app_host
 import service
@@ -9,7 +10,7 @@ def list_users():
     users = service.get_all_users()
     return jsonify(users)
 
-@app.route("/users/<user_id>", methods=['GET'])
+@app.route("/users/<int:user_id>", methods=['GET'])
 def list_user(user_id):
     user = service.get_user(user_id)
     return jsonify(user)
@@ -22,7 +23,7 @@ def add_user():
     except AssertionError as e:
         return { "error":  "{}".format(str(e)) }, 409
 
-@app.route("/quiz/<user_id>", methods=['POST'])
+@app.route("/quiz/<int:user_id>", methods=['POST'])
 def add_quiz(user_id):
     try:
         new_quiz = service.create_quiz(user_id, request.json)
@@ -30,13 +31,14 @@ def add_quiz(user_id):
     except AssertionError as e:
         return { "error":  "{}".format(str(e)) }, 404
     
-@app.route("/distance/<user_id>/<activity_id>/<top>", methods=['GET'])
+@app.route("/distance/<int:user_id>/<int:activity_id>/<int:top>", methods=['GET'])
 def get_nearest(user_id, activity_id, top):
     try:
-        nearest = service.get_nearest_users(user_id, activity_id, int(top))
+        nearest = service.get_nearest_users(user_id, activity_id, top)
         return jsonify(nearest)
     except AssertionError as e:
         return { "error":  "{}".format(str(e)) }, 404
+
 
 # solo per debug
 @app.route("/distance", methods=['GET'])
@@ -48,6 +50,11 @@ def list_distances():
 def list_quizzes():
     return Quiz.query.all()
 
+
+# error handling
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    return jsonify({"error": e.description, "code": e.code}), e.code
 
 
 if __name__ == '__main__':
