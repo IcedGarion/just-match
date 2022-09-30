3fbd26ee68e9b28b6176e26ff4c38237dc70a5c3
garion.musetta <garion.musetta@sysdata.it>
2022-09-30 18:05:30 +0200

    Added normalization to category distance calc

--- src/quiz_distance.py
+++ src/quiz_distance.py
@@ -1,5 +1,5 @@
 import numpy as np
-from db_setup import UserQuiz, Distance
+from db_setup import UserQuiz, Distance, Normalization
 
 # TODO: normalizzazione (serve calcolare il denominatore, come costante)
 def calc_distance(my_quiz, another_users_quiz, category):
@@ -7,7 +7,11 @@ def calc_distance(my_quiz, another_users_quiz, category):
     sue_risposte = [[int(risposta.answer)] for risposta in another_users_quiz ]
     distanze = []
 
-    distanza = np.linalg.norm(np.array(mie_risposte) - np.array(sue_risposte))
+    # legge fattore di normalizzazione per questa categoria
+    normalization_factor = Normalization.query.filter(Normalization.category_id == category).one()
+    
+    # calcola distanza vettoriale
+    distanza = np.linalg.norm(np.array(mie_risposte) - np.array(sue_risposte)) / normalization_factor.value
    
     # crea oggetti distanze da salvare poi su db
     new_distance = Distance(user1_id=my_quiz[0].user_id, user2_id=another_users_quiz[0].user_id, distance=distanza, category_id=category)
